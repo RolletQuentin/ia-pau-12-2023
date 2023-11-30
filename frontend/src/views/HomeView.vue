32d6c748c5f1a7668e4803832b7d013bee7f4b08
RolletQuentin <rollet.quentin@orange.fr>
2023-11-30 21:20:37 +0100

    setup Vue app
    Start to setup Vue app, delete all the things from the quick setup, and modify the API to accecpt origins from the front app

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h2>Hello World from Vue!</h2>
+    <h2>{{ welcomeMessage }}</h2>
 </template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const welcomeMessage = ref('')
+        fetch('http://localhost:8000/')
+            .then((response) => response.json())
+            .then((data) => (welcomeMessage.value = data.message))
+            .catch((error) => console.error(error))
+
+        return {
+            welcomeMessage
+        }
+    }
+}
+</script>

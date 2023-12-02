577a84909ccbedf9903fc4cef0b81f7c1eae19ab
VERGNET-MarcAntoine <mc.ate.vgt@gmail.com>
2023-12-03 04:25:01 +0900

    pour quentin

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from requests import Request
+from airtable_api import get_data_projet_affichage
+from airtable_api import put_new_project
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List
+
+
 
 app = FastAPI()
 
@@ -16,7 +24,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Create_project(BaseModel):
+    nom_du_projet: str
+    description_du_projet: str
+    besoins_actuels: str
+    domaine_activite: str
+    maturite_du_projet: str
+    lieu_du_projet: str
+    code_postal: str
+    odd: List[str]
+    matieres_entrantes: str
+    coproduits: str
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello world from FastAPI!"}
+
+@app.post("/create-project/")
+async def create_project(create_project: Create_project):
+    put_new_project(create_project)
+    return create_project   
+
+@app.get("/links-between-projects/")
+async def links_between_projects():
+    return JSONResponse(content=get_data_projet_affichage())
